0b7c43687c855d192248070491af241e8721decd
Blake Bassett <blkbsstt@gmail.com>
2012-05-09 18:54:57 -0500

    Added license text

--- src/java/edu/ua/eng/software/mallard/XMLPrinter.java
+++ src/java/edu/ua/eng/software/mallard/XMLPrinter.java
@@ -1,3 +1,10 @@
+/*
+ * [The "New BSD" license]
+ * Copyright (c) 2012 The Board of Trustees of The University of Alabama
+ * All rights reserved.
+ *
+ * See LICENSE for details.
+ */
 package edu.ua.eng.software.mallard;
 
 import java.util.concurrent.ConcurrentMap;
